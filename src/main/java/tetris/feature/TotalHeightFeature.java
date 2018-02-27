3e4d11dc4c19ad768853d3127d6dcf67c7eb0640
alexanderleegs <alexleegs@gmail.com>
2018-02-27 15:45:48 +0800

    Added total height feature

---
+++ src/main/java/tetris/feature/TotalHeightFeature.java
@@ -0,0 +1,17 @@
+package tetris.feature;
+
+import tetris.State;
+
+public class TotalHeightFeature extends Feature {
+  @Override
+  public double getValue(State s) {
+  	int highest[] = s.getTop();
+  	int sum = 0;
+  	for (int i : highest) {
+  	  sum += i;
+  	}
+  	return sum;
+  };
+
+
+}
