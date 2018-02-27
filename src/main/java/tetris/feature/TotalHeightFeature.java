9acfb62707a0114ab708b7c6df448f9f12a1f7ae
alexanderleegs <alexleegs@gmail.com>
2018-02-27 16:12:34 +0800

    added unevenness feature

--- src/main/java/tetris/feature/TotalHeightFeature.java
+++ src/main/java/tetris/feature/TotalHeightFeature.java
@@ -6,7 +6,7 @@ public class TotalHeightFeature extends Feature {
   @Override
   public double getValue(State s) {
   	int highest[] = s.getTop();
-  	int sum = 0;
+  	double sum = 0;
   	for (int i : highest) {
   	  sum += i;
   	}
