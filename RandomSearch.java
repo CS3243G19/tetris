65846950c100240dd2eb4795122c03de5c9a34f0
Jethro Kuan <jethrokuan95@gmail.com>
2018-02-27 14:14:43 +0800

    Add random search (#1)

---
+++ RandomSearch.java
@@ -0,0 +1,24 @@
+public class RandomSearch {
+  public int pickMove(State s, int[][] legalMoves) {
+    double rand = Math.random();
+    return (int) Math.floor(rand * legalMoves.length);
+  }
+
+  public static void main(String[] args) {
+    State s = new State();
+    new TFrame(s);
+    RandomSearch p = new RandomSearch();
+    while(!s.hasLost()) {
+      s.makeMove(p.pickMove(s,s.legalMoves()));
+      s.draw();
+      s.drawNext(0,0);
+      try {
+        Thread.sleep(300);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+    System.out.println("You have completed "+s.getRowsCleared()+" rows.");
+  }
+
+}
