db8a1dd65fb2cf1fe1af1e2785de417ccce794d1
alexanderleegs <alexleegs@gmail.com>
2018-02-27 18:11:51 +0800

    added hole feature

--- src/main/java/tetris/feature/HoleFeature.java
+++ src/main/java/tetris/feature/HoleFeature.java
@@ -2,11 +2,19 @@ package tetris.feature;
 
 import tetris.State;
 
-
 public class HoleFeature extends Feature {
   @Override
   public double getValue(State s) {
-    // Please implement
-    return 0;
+    int highest[] = s.getTop();
+    int holes = 0;
+    int[][] field = s.getField();
+    for (int col = 0; col < State.COLS; col++) {
+      for (int row = 0; row < highest[col]; row++) {
+        if (field[row][col] == 0) {
+          holes++;
+        }
+      }
+    }
+    return holes;
   }
 }
