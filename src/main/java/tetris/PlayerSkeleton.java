fbcf1d45db7436f72e6e1cf33df67f1f966c9360
derrick <derrick.2a4@gmail.com>
2018-02-27 18:53:01 +0800

    Added initial runner

--- src/main/java/tetris/PlayerSkeleton.java
+++ src/main/java/tetris/PlayerSkeleton.java
@@ -1,10 +1,31 @@
 package tetris;
 
+import tetris.feature.*;
+import tetris.heuristic.Heuristic;
+
+import java.util.ArrayList;
 
 public class PlayerSkeleton {
-  public int pickMove(State s, int[][] legalMoves) {
+  public int pickMove(State s, int[][] legalMoves, Heuristic h) {
 
-    return 0;
+    double maxValue = Double.MIN_VALUE;
+    int index = -1;
+
+    for (int i = 0; i < legalMoves.length; i++) {
+      State nextState = new State(s);
+      nextState.makeMove(i);
+      if (nextState.hasLost()) {
+        continue;
+      }
+
+      double val = h.getValue(nextState);
+      if (val > maxValue) {
+        maxValue = val;
+        index = i;
+      }
+    }
+
+    return index;
   }
 
   public static void main(String[] args) {
@@ -12,7 +33,13 @@ public class PlayerSkeleton {
     new TFrame(s);
     PlayerSkeleton p = new PlayerSkeleton();
     while(!s.hasLost()) {
-      s.makeMove(p.pickMove(s,s.legalMoves()));
+      ArrayList<Feature> features = new ArrayList<Feature>();
+      features.add(new RowsClearedFeature());
+      features.add(new TotalHeightFeature());
+      features.add(new HoleFeature());
+//      features.add(new UnevenFeature());
+      Heuristic h = new Heuristic(features);
+      s.makeMove(p.pickMove(s,s.legalMoves(), h));
       s.draw();
       s.drawNext(0,0);
       try {
