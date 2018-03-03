02cc5a21ca85d119af1e6f340f2d24f56e8f947f
Jethro Kuan <jethrokuan95@gmail.com>
2018-03-03 10:55:40 +0800

    Implement Scorer (#18)
    * add Scorer
    
    * Fix gradle, update scorer

--- src/main/java/tetris/PlayerSkeleton.java
+++ src/main/java/tetris/PlayerSkeleton.java
@@ -3,41 +3,14 @@ package tetris;
 import tetris.feature.*;
 import tetris.heuristic.Heuristic;
 
+import tetris.scorer.Scorer;
+
 import java.util.ArrayList;
 
 public class PlayerSkeleton {
-  public int pickMove(State s, int[][] legalMoves, Heuristic h) {
-
-    double maxValue = -Double.MAX_VALUE;
-
-    int index = -1;
-
-    for (int i = 0; i < legalMoves.length; i++) {
-      State nextState = new State(s);
-      nextState.makeMove(i);
-
-      if (nextState.hasLost()) {
-        continue;
-      }
-
-      double val = h.getValue(nextState);
-      if (val > maxValue) {
-        maxValue = val;
-        index = i;
-      }
-    }
-
-    // Every move leads to a loss.
-    if (index == -1) {
-      index = 0; // make a random move
-    }
-
-    return index;
-  }
-
   public static void main(String[] args) {
-    State s = new State();
-    new TFrame(s);
+    State state = new State();
+    new TFrame(state);
     PlayerSkeleton p = new PlayerSkeleton();
     ArrayList<Feature> features = new ArrayList<Feature>();
     features.add(new RowsClearedFeature());
@@ -45,19 +18,9 @@ public class PlayerSkeleton {
     features.add(new HoleFeature());
     features.add(new UnevenFeature());
 
-    Heuristic h = new Heuristic(features);
-    while(!s.hasLost()) {
-
-      s.makeMove(p.pickMove(s,s.legalMoves(), h));
-      s.draw();
-      s.drawNext(0,0);
-      try {
-        Thread.sleep(300);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    }
-    System.out.println("You have completed "+s.getRowsCleared()+" rows.");
+    Heuristic heuristic = new Heuristic(features);
+    Scorer scorer = new Scorer(heuristic);
+    scorer.play(true);
+    System.out.printf("Rows cleared: %d", scorer.getLatestScore());
   }
-
 }
---
+++ src/main/java/tetris/scorer/Scorer.java
@@ -0,0 +1,82 @@
+package tetris.scorer;
+
+import tetris.TFrame;
+import tetris.State;
+import tetris.heuristic.Heuristic;
+import tetris.feature.*;
+import tetris.player.Player;
+
+import java.util.ArrayList;
+
+public class Scorer {
+  private static final int SLEEPTIME = 300;
+  protected int game;
+  public ArrayList<Integer> scores;
+  public Player player;
+
+  public Scorer(Heuristic heuristic) {
+    this.player = new Player(heuristic);
+    this.scores = new ArrayList<Integer>();
+    this.game = 0;
+  }
+
+  public void play() {
+    play(false);
+  }
+
+  public void play(boolean graphics) {
+    game++;
+    State state = new State();
+    if (graphics) {
+      new TFrame(state);
+    }
+    while(!state.hasLost()) {
+      int move = player.getMove(state);
+      state.makeMove(move);
+
+      if (graphics) {
+        state.draw();
+        state.drawNext(0,0);
+        try {
+          Thread.sleep(SLEEPTIME);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+
+    int score = state.getRowsCleared();
+    if (!graphics) {
+      System.out.printf("Game %d: %d\n", game, score);
+    }
+    scores.add(score);
+  }
+
+  public int getLatestScore() {
+    return scores.get(scores.size() - 1);
+  }
+
+  public double getAverageScore() {
+    int acc = 0;
+    for (Integer i : scores) acc += i;
+    return (double) acc / scores.size();
+  }
+
+  public static void main(String[] args) {
+    ArrayList<Feature> features = new ArrayList<Feature>();
+    features.add(new RowsClearedFeature());
+    features.add(new TotalHeightFeature());
+    features.add(new HoleFeature());
+    features.add(new UnevenFeature());
+
+    Heuristic heuristic = new Heuristic(features);
+
+    Scorer scorer = new Scorer(heuristic);
+
+    for (int i = 0; i < 100; i++) {
+      scorer.play();
+    }
+
+    System.out.printf("Average Score: %f", scorer.getAverageScore());
+  }
+}
