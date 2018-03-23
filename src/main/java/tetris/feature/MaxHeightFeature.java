38700a980be272df4020419304d9a1f5b8be7bf7
pukipuki <dfwong5566@gmail.com>
2018-03-23 21:08:04 +0800

    added MaxHeightFeature

---
+++ src/main/java/tetris/feature/MaxHeightFeature.java
@@ -0,0 +1,18 @@
+package tetris.feature;
+
+import tetris.NextState;
+
+public class MaxHeightFeature {
+    @Override
+    public double getValue(NextState s) {
+        int maxHeight = 0;
+        int arrayOfHeights[] = s.getTop();
+        for (int i = 0; i < arrayOfHeights.length; i++) {
+            int curr = arrayOfHeights[i];
+            if (curr > maxHeight) {
+                maxHeight = curr;
+            }
+        }
+        return maxHeight;
+    }
+}
