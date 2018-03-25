91c14aef7ac0b3307efd6492e6e5f31c0f3afa04
pukipuki <dfwong5566@gmail.com>
2018-03-25 23:04:58 +0800

    added kply2

--- src/main/java/tetris/player/Player.java
+++ src/main/java/tetris/player/Player.java
@@ -5,6 +5,7 @@ import tetris.State;
 import tetris.heuristic.Heuristic;
 
 public class Player {
+  boolean DEBUG = true;
   public Heuristic heuristic;
 
   public Player(Heuristic heuristic) {
@@ -21,12 +22,17 @@ public class Player {
       NextState nextState = new NextState(s);
 
       nextState.makeMove(i, legalMoves);
+      double averageCloud = spamPly(nextState, 1);
 
       if (nextState.hasLost()) {
         continue;
       }
 
-      double val = heuristic.getValue(nextState);
+      double heuristicValue = heuristic.getValue(nextState);
+      pd("avg Cloud value: " + averageCloud);
+      pd("heuristic value: " + heuristicValue);
+
+      double val = heuristicValue + averageCloud;
       if (val > maxValue) {
         maxValue = val;
         index = i;
@@ -39,5 +45,36 @@ public class Player {
     }
 
     return index;
+  }
+
+  public double spamPly(NextState s, int depth) {
+    double totalValue = 0;
+    // give it all 7 possible pieces
+    for (int i=0; i<7; i++) {
+      NextState nextPiece = new NextState(s);
+      nextPiece.setNextPiece(i);
+      int[][] legalMoves = new State().getLegalMoves(i);
+
+      double maxValue = -Double.MAX_VALUE;
+      for (int j=0; j<legalMoves.length; j++) {
+        NextState nextState = new NextState(nextPiece);
+        nextState.makeMove(j, legalMoves);
+        if (nextState.hasLost()) {
+          continue;
+        }
+        double val = heuristic.getValue(nextState);
+        if (val > maxValue) {
+          maxValue = val;
+        }
+      }
+      totalValue += maxValue;
+    }
+    return totalValue/7.0;
+  }
+
+  public void pd(Object a) {
+    if(DEBUG) {
+      System.out.println(a);
+    }
   }
 }
