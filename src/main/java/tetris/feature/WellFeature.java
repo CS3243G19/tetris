dda12b680b15053b904f0aa51f03b0a42b9d9600
derrick <derrick.2a4@gmail.com>
2018-04-06 14:53:41 +0800

    Fix well feature

--- src/main/java/tetris/feature/WellFeature.java
+++ src/main/java/tetris/feature/WellFeature.java
@@ -4,29 +4,33 @@ import tetris.NextState;
 import tetris.State;
 
 public class WellFeature extends Feature {
-  @Override
-  public double getValue(NextState s) {
-    int wells = 0;
-    int[][] field = s.getField();
-    for (int col = 0; col < State.COLS; col++) {
-      for (int row = 0; row < State.ROWS; row++) {
-        if (field[row][col] == 0) {
-          if (col == 0) {
-            if (field[row][col+1] == 1) {
-              wells++;
+    @Override
+    public double getValue(NextState s) {
+        int wells = 0;
+        int[][] field = s.getField();
+        int[] top = s.getTop();
+        for (int col = 0; col < State.COLS; col++) {
+            if (top[col] == 1) {
+                continue;
             }
-          } else if (col == State.COLS - 1) {
-            if (field[row][col-1] == 1) {
-              wells++;
+            for (int row = 0; row < State.ROWS; row++) {
+                if (field[row][col] == 0) {
+                    if (col == 0) {
+                        if (field[row][col+1] == 1) {
+                            wells++;
+                        }
+                    } else if (col == State.COLS - 1) {
+                        if (field[row][col-1] == 1) {
+                            wells++;
+                        }
+                    } else if (field[row][col - 1] == 1 && field[row][col + 1] == 1) {
+                        wells++;
+                    }
+                }
             }
-          } else if (field[row][col - 1] == 1 && field[row][col + 1] == 1) {
-            wells++;
-          }
         }
-      }
+        return wells;
     }
-    return wells;
-  }
 
   @Override
   public boolean equals(Object o) {
