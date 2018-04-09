f4b457025a4f4f79729a9a89935e87ef31af43f1
Jethro Kuan <jethrokuan95@gmail.com>
2018-04-09 21:32:06 +0800

    simplify heuristic, remove heuristic read and write from file

--- src/main/java/tetris/heuristic/Heuristic.java
+++ src/main/java/tetris/heuristic/Heuristic.java
@@ -19,45 +19,14 @@ import tetris.feature.WellFeature;
 
 
 public class Heuristic {
-    private static final HashMap<Integer, Class<?>> FEATUREMAP = initializeFeatures();
     private final int size;
     private Double[] weights;
     private ArrayList<Feature> features;
 
-    public Heuristic(File file) {
-        features = new ArrayList<Feature>();
-        String[] featureArray = new String[1];
-        String[] weightArray = new String[1];
-        String features;
-        String weights;
-
-        try {
-            FileReader fileReader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            features = bufferedReader.readLine();
-            weights = bufferedReader.readLine();
-            featureArray = features.split(",");
-            weightArray = weights.split(",");
-        } catch (IOException e) {
-            System.out.printf("Unable to read heuristic configuration from file.");
-            e.printStackTrace();
-        }
-
-        this.size = featureArray.length;
-        this.weights = new Double[this.size];
-        for (int i = 0; i < weightArray.length; i++) {
-            this.weights[i] = (Double.parseDouble(weightArray[i]));
-        }
-
-        for (String feat: featureArray) {
-            addFeature(feat);
-        }
-
-        if (this.weights.length != this.features.size()) {
-            System.out.printf("Initialisation went wrong: weight and feature array not equal size");
-        }
-    }
-
+    /**
+     * Initialize new Heuristic with random weights
+     * @param features
+     */
     public Heuristic(ArrayList<Feature> features) {
         this.features = features;
         this.size = this.features.size();
@@ -68,41 +37,15 @@ public class Heuristic {
         }
     }
 
+    /**
+     * Initialize new Heuristic with given weights
+     * @param features
+     * @param heuristicArray
+     */
     public Heuristic(ArrayList<Feature> features, Double[] heuristicArray) {
         this.features = features;
         this.size = this.features.size();
         this.weights = heuristicArray;
-    }
-
-    private static HashMap<Integer, Class<?>> initializeFeatures() {
-        HashMap<Integer, Class<?>> map = new HashMap<Integer, Class<?>>();
-        map.put(1, RowsClearedFeature.class);
-        map.put(2, HoleFeature.class);
-        map.put(3, RowTransitionsFeature.class);
-        map.put(4, ColTransitionsFeature.class);
-        map.put(5, WellFeature.class);
-        return map;
-    }
-
-    public static Integer getFeatureIndex(Feature feature) {
-        for (Map.Entry<Integer, Class<?>> entry: FEATUREMAP.entrySet()) {
-            if(feature.getClass() == entry.getValue()) {
-                return entry.getKey();
-            }
-        }
-        return null;
-    }
-
-    private void addFeature(String feature) {
-        int featIndex = Integer.parseInt(feature);
-        Class<?> feat = FEATUREMAP.get(featIndex);
-        if (feat != null) {
-            try {
-                features.add((Feature) feat.newInstance());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
     }
 
     public double getValue(NextState s) {
