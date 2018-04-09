a9fa5ff91c8cfbfa245850b9ad4825c5e86778ec
Jethro Kuan <jethrokuan95@gmail.com>
2018-04-09 13:05:52 +0800

    make SLEEP_TIME back to 300

--- src/main/java/tetris/scorer/Scorer.java
+++ src/main/java/tetris/scorer/Scorer.java
@@ -8,57 +8,54 @@ import tetris.heuristic.Heuristic;
 import tetris.player.Player;
 
 public class Scorer {
-  private static final int SLEEPTIME = 000;
-  protected int game;
-  public ArrayList<Integer> scores;
-  public Player player;
+    private static final int SLEEPTIME = 300;
+    protected int game;
+    public ArrayList<Integer> scores;
+    public Player player;
 
-  public Scorer(Heuristic heuristic) {
-    this.player = new Player(heuristic);
-    this.scores = new ArrayList<Integer>();
-    this.game = 0;
-  }
-
-  public void play() {
-    play(false);
-  }
-
-  public void play(boolean graphics) {
-    game++;
-    State state = new State();
-    if (graphics) {
-      new TFrame(state);
+    public Scorer(Heuristic heuristic) {
+        this.player = new Player(heuristic);
+        this.scores = new ArrayList<Integer>();
+        this.game = 0;
     }
-    while(!state.hasLost()) {
-      int move = player.getMove(state);
-      state.makeMove(move);
 
-      if (graphics) {
-        state.draw();
-        state.drawNext(0,0);
-        try {
-          Thread.sleep(SLEEPTIME);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
+    public void play() {
+        play(false);
+    }
+
+    public void play(boolean graphics) {
+        game++;
+        State state = new State();
+        if (graphics) {
+            new TFrame(state);
         }
-      }
+        while(!state.hasLost()) {
+            int move = player.getMove(state);
+            state.makeMove(move);
+
+            if (graphics) {
+                state.draw();
+                state.drawNext(0,0);
+                try {
+                    Thread.sleep(SLEEPTIME);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        int score = state.getRowsCleared();
+        scores.add(score);
     }
 
-    int score = state.getRowsCleared();
-    if (!graphics) {
-//      System.out.printf("Game %d: %d\n", game, score);
+    public int getLatestScore() {
+        return scores.get(scores.size() - 1);
     }
-    scores.add(score);
-  }
 
-  public int getLatestScore() {
-    return scores.get(scores.size() - 1);
-  }
-
-  public double getAverageScore() {
-    int acc = 0;
-    for (Integer i : scores) acc += i;
-    return (double) acc / scores.size();
-  }
+    public double getAverageScore() {
+        int acc = 0;
+        for (Integer i : scores) acc += i;
+        return (double) acc / scores.size();
+    }
 
 }
